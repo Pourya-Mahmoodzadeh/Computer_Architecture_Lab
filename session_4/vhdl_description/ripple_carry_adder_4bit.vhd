86dd83142a25030d8d1f24bcee7fbb30e8359258
Pourya Mahmoodzadeh <mpourya01@gmail.com>
2025-11-05 13:24:44 +0330

    create: session_4/vhdl_description/ripple_carry_adder_4bit*

---
+++ session_4/vhdl_description/ripple_carry_adder_4bit.vhd
@@ -0,0 +1,71 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+
+entity ripple_carry_adder_4bit is
+    port (
+        i1, i2 : in std_logic_vector(3 downto 0);
+        sum : out std_logic_vector(3 downto 0);
+        cout : out std_logic
+    );
+end entity;
+
+
+
+architecture rtl of ripple_carry_adder_4bit is 
+
+    component full_adder is 
+        port (
+        i1, i2, i3 : in std_logic;
+        sum, cout : out std_logic
+    );
+    end component full_adder;
+
+    component half_adder is 
+        port (
+        i1, i2 : in std_logic;
+        sum, cout : out std_logic
+    );
+    end component half_adder;
+
+    signal prev_couts : std_logic_vector(2 downto 0);
+
+begin
+
+    sig0 : component half_adder -- sig stands for significance of the inputs and outputs related to half/full adder.
+     port map(
+        i1 => i1(0),
+        i2 => i2(0),
+        sum => sum(0),
+        cout => prev_couts(0)
+    );
+
+    sig1 : component full_adder
+     port map(
+        i1 => i1(1),
+        i2 => i2(1),
+        i3 => prev_couts(0),
+        sum => sum(1),
+        cout => prev_couts(1)
+    );
+
+    sig2 : component full_adder
+     port map(
+        i1 => i1(2),
+        i2 => i2(2),
+        i3 => prev_couts(1),
+        sum => sum(2),
+        cout => prev_couts(2)
+    );
+
+    sig3 : component full_adder
+     port map(
+        i1 => i1(3),
+        i2 => i2(3),
+        i3 => prev_couts(2),
+        sum => sum(3),
+        cout => cout
+    );
+
+end architecture rtl;
---
+++ session_4/vhdl_description/ripple_carry_adder_4bit_tb.vhd
@@ -0,0 +1,63 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+
+entity ripple_carry_adder_4bit_tb is
+end entity ripple_carry_adder_4bit_tb;
+
+
+
+architecture behavior of ripple_carry_adder_4bit_tb is
+
+    component ripple_carry_adder_4bit is
+        port (
+        i1, i2 : in std_logic_vector(3 downto 0);
+        sum : out std_logic_vector(3 downto 0);
+        cout : out std_logic
+    );
+    end component ripple_carry_adder_4bit;
+
+    signal i1 : std_logic_vector(3 downto 0) := "0000";
+    signal i2 : std_logic_vector(3 downto 0) := "0000";
+    signal sum : std_logic_vector(3 downto 0);
+    signal cout : std_logic;
+
+    signal waiting_time : time := 20 ns;
+
+begin
+
+    uut : component ripple_carry_adder_4bit
+     port map(
+        i1 => i1,
+        i2 => i2,
+        sum => sum,
+        cout => cout
+    );
+
+    test : process 
+    begin
+        wait for waiting_time;
+        i1 <= "0001";
+        wait for waiting_time;
+        i1 <= "0010";
+        wait for waiting_time;
+        i2 <= "0001";
+        wait for waiting_time;
+        i2 <= "0010";
+        wait for waiting_time;
+        i1 <= "1001";
+        i2 <= "0110";
+        wait for waiting_time;
+        i1 <= "1010";
+        i2 <= "1010";
+        wait for waiting_time;
+        i1 <= "1111";
+        i2 <= "1111";
+        wait for waiting_time;
+        i1 <= "0001";
+        wait for waiting_time;
+        wait;
+    end process;
+end architecture behavior;
