b4e3e560efdefa7296d5f6ac79323a6487a745d4
Pourya Mahmoodzadeh <mpourya01@gmail.com>
2026-01-10 11:11:34 +0330

    minor renaming and modifications.

--- session_6/vhdl_description/register/minimal/generic/minimal_reg_nbit.vhd
+++ session_6/vhdl_description/register/minimal/generic/minimal_reg_nbit.vhd
@@ -6,12 +6,12 @@ use ieee.numeric_std.all;
 
 entity minimal_reg_nbit is
     generic (
-        n : integer := 8
+        WIDTH : integer := 8
     );
     port(
         clear_n, clk : in std_logic;
-        data : in std_logic_vector((n - 1) downto 0);
-        Q, Q_n : out std_logic_vector((n - 1) downto 0)
+        data : in std_logic_vector((WIDTH - 1) downto 0);
+        Q, Q_n : out std_logic_vector((WIDTH - 1) downto 0)
     );
 end entity;
 
@@ -21,8 +21,8 @@ architecture rtl of minimal_reg_nbit is
 
 begin
 
-    CONNECT_WIRES: for i in (n - 1) downto 0 generate
-        D_FF_INS : entity work.D_FF -- address: session_6/vhdl_description/flip_flop/minimal/behavioral/D_FF.vhd
+    CONNECT_WIRES: for i in (WIDTH - 1) downto 0 generate
+        D_FF_INS : entity work.minimal_D_FF -- address: session_6/vhdl_description/flip_flop/minimal/behavioral/D_FF.vhd
             port map(
                 reset_n => clear_n,
                 clk => clk,
